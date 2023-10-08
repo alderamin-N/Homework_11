a563c6260cfe65ce8f43afcacafa0725d5bb3d14
NPototskaya <kusochek-91@mail.ru>
2023-10-08 12:16:59 +0300

    Add project files.

---
+++ Homework_11/OtusDictionary.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework_11
+{
+    public class OtusDictionary
+    {
+        private string?[] _array = new string[32];
+        public string? this[int i]
+        {
+            get { return Get(i); }
+            set { Add(i, value); }
+        }
+        public void Add(int key, string? value)
+        {
+           if (value == null)
+            {
+                throw new InvalidOperationException("Словарь не может сохранять null");
+            }
+           while(key > _array.Length)
+            {
+                string[] newArray = new string[_array.Length * 2];
+                _array.CopyTo(newArray, 0);
+                _array = newArray;
+            }            
+            if (_array[key] != null)
+            {
+                throw new InvalidOperationException("Место уже занято!");
+            }
+            _array[key] = value;
+        }
+
+        public string? Get(int key)
+        { 
+            if (key < _array.Length)
+            {
+                return _array[key];
+            }
+            return null;
+        }
+    }
+}
---
+++ Homework_11/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace Homework_11
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            OtusDictionary array = new OtusDictionary();
+            array.Add(2, "значение 1");
+            array.Add(5, "значение 2");
+            //array.Add(2, "kjj");
+            array.Add(10, "значение 3");
+            array.Add(130, "значение 5");
+            array[6] = "5"; ;
+            
+            Console.WriteLine(array.Get(13));
+            Console.WriteLine(array.Get(5));
+            Console.WriteLine(array[6]);
+            
+        }
+    }
+}
